d8829c110e0801c9c69c0a871574ec2a906d038d
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-11-24 21:14:00 -0300

    feat: adicionar documentação da API com Swagger e configurar rota para acesso

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,17 @@
+
 import express from 'express';
 import authRoutes from './routes/auth.routes';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+
+const swaggerPath = path.join(__dirname, '../swagger.yaml');
+const swaggerDocument = YAML.load(swaggerPath);
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
